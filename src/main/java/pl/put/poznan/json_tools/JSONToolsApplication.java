0ccee459d4a4b6d12c04147adad539c3f945a2b8
Przemysław Marcinkowski <przemekmarcinkowski2000@gmail.com>
2022-12-09 22:30:07 +0100

    Remove unneded files

--- src/main/java/pl/put/poznan/json_tools/JSONToolsApplication.java
+++ src/main/java/pl/put/poznan/json_tools/JSONToolsApplication.java
@@ -1,10 +1,10 @@
-package pl.put.poznan.json_tools.app;
+package pl.put.poznan.json_tools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
+@SpringBootApplication
 public class JSONToolsApplication{
 
     public static void main(String[] args) {
